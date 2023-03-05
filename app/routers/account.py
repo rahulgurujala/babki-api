8bc5285774607f0700c3ab0c8426198b65291a73
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-03-05 23:38:12 +0300

    Update user and account routes

--- app/routers/account.py
+++ app/routers/account.py
@@ -1,39 +1,92 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.orm import Session
 
 from app.database import get_db
 
-from .. import models
-from ..schemas import *
+from .. import models, oauth2, schemas
 
-router = APIRouter(prefix="/user", tags=["Accounts"])
+router = APIRouter(prefix="/accounts", tags=["Accounts"])
 
 
-@router.post("/account", status_code=status.HTTP_201_CREATED)
-def create_account(Account: AccountIn, db: Session = Depends(get_db)):
+@router.get("/", status_code=status.HTTP_200_OK, response_model=list[schemas.Account])
+def get_accounts(user_id: int, db: Session = Depends(get_db)):
+    """Returns all user accounts"""
+
+    accounts = db.query(models.Account).filter(models.Account.user_id == user_id).all()
+
+    return accounts
+
+
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.Account)
+def get_account(id: int, db: Session = Depends(get_db)):
+    """Return single user account"""
+
+    account = db.query(models.Account).filter(models.Account.id == id).first()
+
+    if not account:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Account does not exist."
+        )
+
+    return account
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Account)
+def create_account(
+    Account: schemas.AccountCreate,
+    db: Session = Depends(get_db),
+    user_id: int = Depends(oauth2.get_current_user),
+):
     """Creates user account"""
 
     user = db.query(models.User).filter(models.User.id == Account.user_id).first()
-    print(dir(user))
 
-    user.accounts.append(models.Account(**Account.dict()))
-    db.add(user)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="User does not exist."
+        )
+
+    account = models.Account(**Account.dict(), owner=user)
+    db.add(account)
     db.commit()
-    db.refresh(user)
-    # print(user)
+    db.refresh(account)
 
-    # accounts = models.Account(owner=user, **Account.dict())
-    # db.add(accounts)
-    # db.commit()
-    # db.refresh(accounts)
-
-    return {"data": user}
+    return account
 
 
-@router.get("/accounts", status_code=status.HTTP_200_OK)
-def get_accounts(db: Session = Depends(get_db)):
-    """Gets all user accounts"""
+@router.patch("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.Account)
+def update_account(
+    id: int, account: schemas.AccountUpdate, db: Session = Depends(get_db)
+):
+    """Updates account"""
 
-    accounts = db.query(models.Account).all()
+    post_query = db.query(models.Account).filter(models.User.id == id)
 
-    return accounts
+    if not post_query.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Account does not exist.",
+        )
+
+    post_query.update(account.dict(exclude_unset=True), synchronize_session=False)
+    db.commit()
+
+    return post_query.first()
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_account(id: int, db: Session = Depends(get_db)):
+    """Deletes user account"""
+
+    post_query = db.query(models.Account).filter(models.User.id == id)
+
+    if not post_query.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Account does not exist.",
+        )
+
+    post_query.delete(synchronize_session=False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,15 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.orm import Session
 
 from app.database import get_db
 
-from .. import models
-from ..schemas import *
+from .. import models, schemas
 
-router = APIRouter(tags=["Users"])
+router = APIRouter(prefix="/users", tags=["Users"])
 
 
-@router.get("/users", status_code=status.HTTP_200_OK)
+@router.get("/", status_code=status.HTTP_200_OK, response_model=list[schemas.User])
 def get_users(db: Session = Depends(get_db)):
     """Returns all users"""
 
@@ -18,28 +17,28 @@ def get_users(db: Session = Depends(get_db)):
     return users
 
 
-@router.get("/user/{id}", status_code=status.HTTP_200_OK)
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.User)
 def get_user(id: int, db: Session = Depends(get_db)):
     """Returns single user"""
 
     user = db.query(models.User).filter(models.User.id == id).first()
 
     if not user:
-        return HTTPException(
+        raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="User does not exist."
         )
 
     return user
 
 
-@router.post("/user", status_code=status.HTTP_201_CREATED)
-def create_user(User: UserIn, db: Session = Depends(get_db)):
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.User)
+def create_user(User: schemas.UserCreate, db: Session = Depends(get_db)):
     """Creates user"""
 
     user = db.query(models.User).filter(models.User.email == User.email).first()
 
     if user:
-        return HTTPException(
+        raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists."
         )
 
@@ -49,4 +48,40 @@ def create_user(User: UserIn, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
 
-    return {"data": new_user}
+    return new_user
+
+
+@router.patch("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.User)
+def update_user(id: int, user: schemas.UserUpdate, db: Session = Depends(get_db)):
+    """Updates user"""
+
+    user_query = db.query(models.User).filter(models.User.id == id)
+
+    if not user_query.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User does not exist.",
+        )
+
+    user_query.update(user.dict(exclude_unset=True), synchronize_session=False)
+    db.commit()
+
+    return user_query.first()
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id: int, db: Session = Depends(get_db)):
+    """Deletes user account"""
+
+    user_query = db.query(models.User).filter(models.User.id == id)
+
+    if not user_query.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User does not exist.",
+        )
+
+    user_query.delete(synchronize_session=False)
+    db.commit()
+
+    return Response()
