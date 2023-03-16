206b0708b554ff4ef2a74d7bf9483088ea7e72d8
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-03-17 00:32:16 +0300

    Edit token schema

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -5,12 +5,3 @@ from pydantic import BaseModel
 from .account import *
 from .transaction import *
 from .user import *
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    id: int
