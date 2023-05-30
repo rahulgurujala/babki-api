43230a1125e609911ef2a658ceb3a93c09e93be9
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-05-31 01:39:39 +0300

    chore: update transaction schema

--- app/schemas/transaction.py
+++ app/schemas/transaction.py
@@ -3,31 +3,28 @@ from typing import Optional
 
 from pydantic import BaseModel
 
-from app.models.transaction import Category
+from app.models.transaction import CategoryType
 
 
 class TransactionBase(BaseModel):
-    user_id: int
-
-
-class TransactionCreate(TransactionBase):
     amount: float
     is_debit: bool
     account_id: int
 
 
-class TransactionUpdate(TransactionBase):
+class TransactionCreate(TransactionBase):
+    pass
+
+
+class TransactionUpdate(BaseModel):
     amount: Optional[float]
-    category: Optional[Category]
+    category: Optional[CategoryType]
 
 
 class Transaction(TransactionBase):
     id: int
-    amount: float
-    is_debit: bool
-    account_id: int
     user_id: int
-    category: Category
+    category: CategoryType
     created_at: datetime
     account_balance: Optional[float]
 
