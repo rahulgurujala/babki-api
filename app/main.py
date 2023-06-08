b36498ef4743d73ed0ce5e49e74d45c59be10464
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-06-08 04:17:13 +0300

    fix: add localhost cors origin

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from routers import account, auth, transaction, user
 
 app = FastAPI(docs_url="/", title="denarii API")
 
-origins = ["*"]
+origins = ["http://localhost:5173"]
 
 app.add_middleware(
     CORSMiddleware,
